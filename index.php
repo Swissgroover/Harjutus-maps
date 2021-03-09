6f68ff41bfdde5eaa287a3c6674e7da5cc416707
Swissgroover <56125454+Swissgroover@users.noreply.github.com>
2021-03-09 22:39:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+    $config = require_once 'config.php';
+    $config = require_once 'db.php';
+    $config = require_once 'locations_model.php';
+
+    function connection ($config) {
+        $dsn = "mysql:host=" . $config['host'] . ";dbname=" . $config['database'];
+
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        try {
+             return new PDO($dsn, $config['username'], $config['password'], $options);
+        } catch (\PDOException $e) {
+             throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+<?
 <!DOCTYPE html>
 <html>
   <head>
